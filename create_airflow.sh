4a0cc83424f6cfa4e72dcce3cb49c8872e3f0f64
Guilherme Lopes <guilherme.lopes@engenharia.ufjf.br>
2021-09-22 15:58:37 -0300

    criando branch de desenvolvimento e configurando airflow

--- create_airflow.sh
+++ create_airflow.sh
@@ -1,3 +1,4 @@
 kubectl create namespace airflow
 helm repo add apache-airflow https://airflow.apache.org
 helm show values apache-airflow/airflow > k8s/myvalues.yaml
+helm install airflow apache-airflow/airflow -f k8s/myvalues.yml -n airflow --debug
